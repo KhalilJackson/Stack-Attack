680301683f4e40025f828bf8bb0b95f9a26f74ce
Khalil Jackson <kjackson@bowdoin.edu>
2023-04-06 20:43:42 -0400

    Checkpoint Reached

---
+++ glow_inject.d
@@ -0,0 +1,9 @@
+
+glow_inject.o:     file format elf64-x86-64
+
+
+Disassembly of section .text:
+
+0000000000000000 <.text>:
+   0:	48 c7 c7 48 8e ca 4b 	mov    $0x4bca8e48,%rdi
+   7:	c3                   	retq   
